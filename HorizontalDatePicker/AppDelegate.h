7ecf160c5669d9feb27a77978dcb8213e4dc519a
Partha Sarathi Reddy Bijjam <partha@ecoihub.com>
2019-02-02 13:46:54 +0530

    Initial Commit

---
+++ HorizontalDatePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorizontalDatePicker
+//
+//  Created by Partha Sarathi Reddy Bijjam on 02/02/19.
+//  Copyright © 2019 Partha Sarathi Reddy Bijjam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
